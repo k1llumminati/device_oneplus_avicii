62c230a38ece6edb14a7da740149b4156417b9cc
Jan Breig <git@pygos.space>
2024-11-04 20:01:23 +0100

    avicii: Build libcamera2ndk_vendor from source

--- device.mk
+++ device.mk
@@ -115,6 +115,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk:64 \
     libcamera_metadata_shim \
     libutilscallstack.vendor:64 \
     vendor.qti.hardware.camera.postproc@1.0.vendor
