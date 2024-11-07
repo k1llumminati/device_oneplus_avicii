58a658afea8f594dc4063784e6fa3f2bab4b7507
Taher Hakim <85365433+TAHERHAKIM@users.noreply.github.com>
2024-11-07 22:47:26 +0400

    Fix QTI USB

--- device.mk
+++ device.mk
@@ -447,7 +447,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
